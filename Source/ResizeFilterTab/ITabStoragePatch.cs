728c83e56a288837b9920f5aa01c826166a34b88
nonnonstop <42905588+nonnonstop@users.noreply.github.com>
2025-08-24 23:51:16 +0900

    Support for pen marker

--- Source/ResizeFilterTab/ITabStoragePatch.cs
+++ Source/ResizeFilterTab/ITabStoragePatch.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 namespace ResizeFilterTab
 {
+    public static class ResizeHelper
+    {
+        public static void ApplyResize(ref Vector2 winSize, ref Vector2 size)
+        {
+            var settings = ResizeFilterTab.Instance.Settings;
+            winSize.x = settings.StorageWidth;
+            winSize.y = settings.StorageHeight;
+            size.x = settings.StorageWidth;
+            size.y = settings.StorageHeight;
+        }
+    }
+
     [HarmonyPatch(typeof(ITab_Storage), nameof(ITab_Storage.OnOpen))]
-    public static class ITabPatch
+    public static class ITabStoragePatch
     {
         public static void Prefix(ref Vector2 ___WinSize, ref Vector2 ___size)
         {
-            var settings = ResizeFilterTab.Instance.Settings;
-            ___WinSize.x = settings.StorageWidth;
-            ___WinSize.y = settings.StorageHeight;
-            ___size.x = settings.StorageWidth;
-            ___size.y = settings.StorageHeight;
+            ResizeHelper.ApplyResize(ref ___WinSize, ref ___size);
+        }
+    }
+
+    [HarmonyPatch(typeof(ITab_PenAnimals), nameof(ITab_PenAnimals.OnOpen))]
+    public static class ITabPenAnimalsPatch
+    {
+        public static void Prefix(ref Vector2 ___WinSize, ref Vector2 ___size)
+        {
+            ResizeHelper.ApplyResize(ref ___WinSize, ref ___size);
+        }
+    }
+
+    [HarmonyPatch(typeof(ITab_PenAutoCut), nameof(ITab_PenAutoCut.OnOpen))]
+    public static class ITabPenAutoCutPatch
+    {
+        public static void Prefix(ref Vector2 ___WinSize, ref Vector2 ___size)
+        {
+            ResizeHelper.ApplyResize(ref ___WinSize, ref ___size);
         }
     }
 }
